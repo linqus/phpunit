fd080bc59b70afeb38069ddef16c58e35afa4726
Karol Linkiewicz <karol@linkiewicz.pl>
2023-08-24 22:34:50 +0200

    dinos tests (sizedescription, health)

--- src/Entity/Dinosaur.php
+++ src/Entity/Dinosaur.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity;
 
+use App\Enum\HealthStatus;
+
 class Dinosaur
 {
     private string $name;
     private string $genus;
     private int $length;
     private string $enclosure;
+    private HealthStatus $health = HealthStatus::HEALTHY;
 
     public function __construct(string $name, string $genus = 'Unknown', int $length = 0, string $enclosure = 'Unknown')
     {
@@ -35,5 +38,31 @@ class Dinosaur
     public function getEnclosure(): string
     {
         return $this->enclosure;
+    }
+
+    public function getSizeDescription(): string
+    {
+        if ($this->length >= 10) {
+            return 'Large';
+        } else if ($this->length < 5) {
+            return 'Small';
+        } else {
+            return 'Medium';
+        };
+        
+       
+    }
+
+    public function isAcceptingVisitors(): bool
+    {
+        return $this->health === HealthStatus::HEALTHY;
+    }
+
+    public function setHealth(HealthStatus $health): void
+    {
+        
+        
+        $this->health = $health;
+        
     }
 }
--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Unit\Entity;
 
 use App\Entity\Dinosaur;
+use App\Enum\HealthStatus;
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
@@ -18,5 +20,40 @@ class DinosaurTest extends TestCase
         self::assertSame(15, $dino->getLength());
         self::assertSame('Paddock A', $dino->getEnclosure());        
 
+    }
+
+    /**
+     *  @dataProvider sizeDescriptionProvider
+     */
+    public function testDinoHasCorrectSizeDescriptionFromLength(int $length, string $expectedSize): void 
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: $length);
+
+        self::assertSame($expectedSize, $dino->getSizeDescription());
+    }
+
+
+    public function sizeDescriptionProvider(): Generator
+    {
+        yield '10 Meter Large Dino' => [10, 'Large'];
+        yield '5 Meter Medium Dino' => [5, 'Medium'];
+        yield '4 Meter Small Dino' => [4, 'Small'];
+    }
+
+    public function testIsAcceptingVisitorsByDefault(): void
+    {
+        $dino = new Dinosaur('Dennis');
+
+        self::assertTrue($dino->isAcceptingVisitors());
+
+
+    }
+
+    public function testIsNotAcceptingVisitorsIfSick(): void
+    {
+        $dino = new Dinosaur('Bumpy');
+        $dino->setHealth(HealthStatus::SICK);
+
+        self::assertFalse($dino->isAcceptingVisitors());
     }
 }
